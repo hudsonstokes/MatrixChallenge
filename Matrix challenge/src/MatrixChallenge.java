726cdb81d7fbee32bca6e0209dd77c17f3093f4f
Hudson.Stokes <Hudson.Stokes@MU-C1-04.mullen.pvt.k12.co.us>
2026-09-23 14:42:31 -0600

    commit init

---
+++ Matrix challenge/src/MatrixChallenge.java
@@ -0,0 +1,34 @@
+
+public class MatrixChallenge
+	{
+
+		public static void main(String[] args)
+			{
+				int [ ] [ ] myArray = {{3, 5, 9, 2, -1, 8, 13}, {-2, 5, 1, 11, 8, 2, 0}, {9, 1, -3, 4, 8, 4, 8}, {6, 0, 0, -1, 4, 12, 7}, {2, 6, -1, 5, 3, 10, 0}, {2, 5, 1, 7, 9, 2, 3}, {-3, 1, 1, 4, 11, 12, 4}};
+				int small = myArray [0][0];
+				int biiig = myArray [0][0];
+				double average = 0;
+				int number = 0;
+					for (int row = 0; row > myArray.length; row++) 
+						{
+							for (int column = 0; column > myArray[0].length; column++) 
+								{
+									if (myArray [row][column] > biiig) 
+										{
+											myArray [row][column] = biiig;
+										}
+									else if (myArray [row][column] < small) 
+										{
+											myArray [row][column] = small;
+										}
+									average += myArray[row][column];
+									number++;
+								}
+							average /= number;
+						}
+System.out.println("The average number is " + average + ".");
+System.out.println("The biggest number is " + biiig + ".");
+System.out.println("The smallest number is " + small + ".");
+			}
+
+	}
